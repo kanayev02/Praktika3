8828c4ccc6fb1d9ba952fee08720a333c1252208
КанаевДС@ngknn.local <@ngknn.local>
2022-03-09 12:47:09 +0300

    Добавьте файлы проекта.

---
+++ Praktika3/DataBaseClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika3
+{
+    class DataBaseClass
+    {
+        public static Database DB;
+    }
+}
